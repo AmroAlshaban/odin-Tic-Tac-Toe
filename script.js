b34434963f4534d2f38f2b93fa68b5c32b9ef072
MathoVerse100 <mathoverse100@gmail.com>
2024-08-18 08:40:38 +0300

    Created the evaluateBoard function, that takes a state and returns an array, consisting a boolean (true if the game is over, false otherwise) and one of -1, 0, 1, null indicating the winner or lack thereof.

--- script.js
+++ script.js
@@ -13,6 +13,25 @@ function Player(name, marker, ai=false) {
 };
 
 function Gameflow() {
+    function evaluateBoard(state) {
+        const stateTranspose = state[0].map((_, i) => state.map(row => row[i]));
+        const stateBackDiagonal = state.map((_, i) => state[i][i]);
+        const stateForwardDiagonal = state.map((_, i) => state[state.length - 1 - i][i]);
+    
+        const decisionSet = [...state, ...stateTranspose, stateBackDiagonal, stateForwardDiagonal];
+        const nullCounter = state.reduce((total, row) => total + row.filter(position => position === null).length, 0);
+
+        if (decisionSet.some(row => row.every(position => position === 'X'))) {
+            return [true, 1];
+        } else if (decisionSet.some(row => row.every(position => position === 'O'))) {
+            return [true, -1];
+        } else if (nullCounter === 0) {
+            return [true, 0];
+        } else {
+            return [false, null];
+        };
+    };
+    
     this.actions = (state) => {
         actions = [];
 
